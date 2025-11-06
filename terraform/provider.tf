5b4419472c149bbe5e7ce56c8d32c5da1e449870
bibek <bibek2003@gmail.com>
2025-11-07 01:15:35 +0530

    empty accountr issue

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,7 +1,7 @@
 provider "snowflake" {
   # Pass these via -var from your workflow
   account  = var.snowflake_account      # e.g., UE47735
-  region   = var.snowflake_region       # e.g., europe-west4.gcp
+  #region   = var.snowflake_region       # e.g., europe-west4.gcp
   user     = var.snowflake_user         # e.g., CICD_BOT
   password = var.snowflake_password
   role     = var.snowflake_role         # e.g., CICD_SNOWFLAKE_DEPLOY
