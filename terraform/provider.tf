52ffa325b957fdc58c12383ba918a0047527cbb7
bibek <bibek2003@gmail.com>
2025-11-06 21:13:31 +0530

    fixing terraform provider issue

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,13 +1,4 @@
-terraform {
-  required_providers {
-    snowflake = {
-      source  = "Snowflake-Labs/snowflake"
-      version = ">= 0.96.0"
-    }
-  }
-  required_version = ">= 1.6.0"
-}
-
-# Provider reads auth & context from environment variables:
-#   SNOWFLAKE_ACCOUNT, SNOWFLAKE_USER, SNOWFLAKE_PASSWORD, SNOWFLAKE_ROLE, SNOWFLAKE_REGION
+# Auth/context come from env:
+# SNOWFLAKE_ACCOUNT, SNOWFLAKE_USER, SNOWFLAKE_PASSWORD, SNOWFLAKE_ROLE
+# (and optionally SNOWFLAKE_REGION if you use locator instead of ORG-ACCOUNT)
 provider "snowflake" {}
