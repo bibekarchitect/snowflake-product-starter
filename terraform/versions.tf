d733818fe908094c3f39581765a29083edd18d82
bibek <bibek2003@gmail.com>
2025-11-07 02:19:16 +0530

    empty accountr issue

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,10 +1,8 @@
 terraform {
-  required_version = ">= 1.6.0"
-
   required_providers {
     snowflake = {
-      source  = "Snowflake-Labs/snowflake"
-      version = ">= 1.6.0"
+      source  = "snowflakedb/snowflake"
+      version = ">= 1.0.0"
     }
   }
 }
