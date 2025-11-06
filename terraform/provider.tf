a16d8adafe26231939c64d71efc2138af61ef3df
bibek <bibek2003@gmail.com>
2025-11-06 23:28:55 +0530

    fixing terraform provider issue

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,6 +1,6 @@
-# Provider relies on environment variables only:
-# SNOWFLAKE_ACCOUNT  -> UE47735
-# SNOWFLAKE_REGION   -> europe-west4.gcp
-# SNOWFLAKE_HOST     -> UE47735.europe-west4.gcp.snowflakecomputing.com
-# SNOWFLAKE_USER, SNOWFLAKE_PASSWORD, SNOWFLAKE_ROLE
-provider "snowflake" {}
+# Use the TOML profile written in the workflow:
+#   file:  $HOME/.snowflake/config
+#   block: [ci]
+provider "snowflake" {
+  profile = "ci"
+}
