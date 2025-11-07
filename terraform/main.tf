6a05ac261f9ffdbb2546f4a96c9eee55e7a45ce7
bibek <bibek2003@gmail.com>
2025-11-07 21:14:30 +0530

    empty accountr issue

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
-resource "snowflake_database" "db" {
-  count = var.create_database ? 1 : 0
-  name  = var.database_name
-}
+# resource "snowflake_database" "db" {
+#   count = var.create_database ? 1 : 0
+#   name  = var.database_name
+# }
 
 resource "snowflake_warehouse" "ingest" {
   name             = var.warehouses.ingest
@@ -11,18 +11,18 @@ resource "snowflake_warehouse" "ingest" {
   resource_monitor = length(var.resource_monitor_name) > 0 ? var.resource_monitor_name : null
 }
 
-resource "snowflake_warehouse" "transform" {
-  name             = var.warehouses.transform
-  warehouse_size   = "SMALL"
-  auto_suspend     = 60
-  auto_resume      = true
-  resource_monitor = length(var.resource_monitor_name) > 0 ? var.resource_monitor_name : null
-}
+# resource "snowflake_warehouse" "transform" {
+#   name             = var.warehouses.transform
+#   warehouse_size   = "SMALL"
+#   auto_suspend     = 60
+#   auto_resume      = true
+#   resource_monitor = length(var.resource_monitor_name) > 0 ? var.resource_monitor_name : null
+# }
 
-resource "snowflake_warehouse" "serve" {
-  name             = var.warehouses.serve
-  warehouse_size   = "XSMALL"
-  auto_suspend     = 60
-  auto_resume      = true
-  resource_monitor = length(var.resource_monitor_name) > 0 ? var.resource_monitor_name : null
-}
+# resource "snowflake_warehouse" "serve" {
+#   name             = var.warehouses.serve
+#   warehouse_size   = "XSMALL"
+#   auto_suspend     = 60
+#   auto_resume      = true
+#   resource_monitor = length(var.resource_monitor_name) > 0 ? var.resource_monitor_name : null
+# }
